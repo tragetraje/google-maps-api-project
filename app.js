5b4e3d385f889786580ee75356fdfdb86c0c7dcd
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-08-18 21:12:05 +1000

    Customize marker color when hoovering on and over

--- app.js
+++ app.js
@@ -9,10 +9,212 @@ var map;
 var markers = [];
 
 function initMap() {
+    // create a styles array to use with the map
+    var styles = [
+      {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#19a0d8"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "weight": 6
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#e85113"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#efe9e4"
+            },
+            {
+                "lightness": -40
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#efe9e4"
+            },
+            {
+                "lightness": -20
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "lightness": 100
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": -100
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.icon"
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "lightness": 20
+            },
+            {
+                "color": "#efe9e4"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "lightness": 100
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": -100
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "hue": "#11ff00"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "lightness": 100
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "hue": "#4cff00"
+            },
+            {
+                "saturation": 58
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#f0e4d3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#efe9e4"
+            },
+            {
+                "lightness": -25
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#efe9e4"
+            },
+            {
+                "lightness": -10
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+    ];
+
     // constructor adds a new map, center and zoom are required parameters
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -32.929927, lng: 151.773169 },
-        zoom: 16
+        zoom: 16,
+        styles: styles,
+        mapTypeControl: false
     });
 
     // these are the coffee shops which are going to be shown to the user
@@ -29,6 +231,13 @@ function initMap() {
     // create infowindow object for a marker to display information, pics etc.
     var largeInfowindow = new google.maps.InfoWindow();
 
+    // style the markers, this is a listing marker icon
+    var defaultIconColor = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    // "highlighted location" marker color for when the user mouses over the
+    // marker
+    var highlightedIconColor = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+
+
     // create an array of markers for the locations
     for (var i = 0; i < locations.length; i++) {
         // get the position from the location array
@@ -39,6 +248,7 @@ function initMap() {
             //map: map,
             position: position,
             title: title,
+            icon: defaultIconColor,
             animation: google.maps.Animation.DROP,
             id: i
         });
@@ -48,6 +258,14 @@ function initMap() {
         // create an onclick event to open an infowindow at each marker
         marker.addListener('click', function() {
           populateInfoWindow(this, largeInfowindow);
+        });
+        // two event listeners to change the color of the marker - one for
+        // mouseover and for mouseout
+        marker.addListener('mouseover', function() {
+          this.setIcon(highlightedIconColor);
+        });
+        marker.addListener('mouseout', function() {
+          this.setIcon(defaultIconColor);
         });
     }
     // create the event listeners to show and hide the listings when the button
