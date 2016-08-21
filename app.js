ea57cb7a141030dd34372634d3501848d926a929
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-08-21 11:34:08 +1000

    Find the location on the map from user input

--- app.js
+++ app.js
@@ -212,6 +212,11 @@ function initMap() {
     // is clicked
     document.getElementById('show-listings').addEventListener('click', showListings);
     document.getElementById('hide-listings').addEventListener('click', hideListings);
+
+    // event listener to zoom the area from user input
+    document.getElementById('zoom-to-area').addEventListener('click', function() {
+      zoomToArea();
+    });
   }
 
     // this function populates the infowindow when the marker is clicked. We'll
@@ -281,5 +286,34 @@ function initMap() {
     function hideListings() {
       for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
+      }
+    }
+
+    // this function takes the input value in the find nearby area text input
+    // locates it, and then zooms into that area. This is so that the user can
+    // show all listings, then decide to focus on one area of the map.
+    function zoomToArea() {
+      // Initialize the geocoder.
+      var geocoder = new google.maps.Geocoder();
+      // Get the address or place that the user entered.
+      var address = document.getElementById('zoom-to-area-text').value;
+      // Make sure the address isn't blank.
+      if (address == '') {
+        window.alert('You must enter an area, or address.');
+      } else {
+        // Geocode the address/area entered to get the center. Then, center the map
+        // on it and zoom in
+        geocoder.geocode(
+          { address: address,
+            componentRestrictions: {locality: 'Newcastle, NSW, AU'}
+          }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              map.setCenter(results[0].geometry.location);
+              map.setZoom(15);
+            } else {
+              window.alert('We could not find that location - try entering a more' +
+                  ' specific place.');
+            }
+          });
       }
     }
